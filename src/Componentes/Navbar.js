4e0a28ffb116d35ef0ce48d0ed6556bd37d9b8c5
dstefaniapaniagua <deb.s.paniagua@gmail.com>
2021-05-13 17:42:54 -0300

    agregando botones al Navbar y corrigiendo errores

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography, Button} from "@material-ui/core";
-import { MenuIcon} from "@material-ui/icons/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 
 
 const Navbar = () => {
     return (
-        <AppBar position="static">
+        <AppBar position="static" style={{ background: "grey"}}>
             <Toolbar>
             <IconButton edge="start" color="inherit" aria-label="menu">
                 <MenuIcon />
             </IconButton>
             <Typography variant="h6">
-                News
+                LEARN
             </Typography>
-            <Button style={{ background: "#0f0" }}>Login</Button>
+            <div style={{ marginLeft: "auto"}} >
+                <Button style={{ color: "white"}}>Authors</Button>
+                <Button style={{ color: "white"}}>E-books</Button>
+                <Button style={{ color: "white"}}>Articles</Button>
+                <Button style={{ background: "black", color: "white"}}>Login</Button>
+            </div>
             </Toolbar>
         </AppBar>
     );
 };
-
 export default Navbar;
