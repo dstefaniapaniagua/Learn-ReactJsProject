6fe99cada9c2a819cbfafbcfd40b505b54557387
dstefaniapaniagua <deb.s.paniagua@gmail.com>
2021-05-20 19:28:49 -0300

    add ItemCount component

---
+++ src/Componentes/ItemCount.js
@@ -0,0 +1,43 @@
+import React from "react"; 
+import { useState } from "react";
+import AddIcon from '@material-ui/icons/AddCircle';
+import RemoveIcon from '@material-ui/icons/RemoveCircle';
+import '../Styles/itemCount.css';
+
+export default function ItemCounter({ stock, initial, onAdd }) {
+    const [contador, setContador] = useState(initial);
+
+    const increase = () => {
+        if (contador < stock) {
+          setContador((contador) => {
+            let cont = contador + 1;
+            onAdd(cont);
+            return cont;
+          });
+        }
+      };
+      
+      const substract = () => {
+        if ( contador > initial) {
+            setContador((contador) => {
+                let cont = contador - 1;
+                onAdd(cont);
+                return cont;
+            });
+        }
+    }
+    
+    return(
+        <div className="ItemCounter">
+            <div className="Contador">
+            <button onClick={substract}>
+                <RemoveIcon style={{ color: 'purple'}}/>
+            </button>
+            <input type="number" value={contador}/>
+            <button onClick={increase}>
+                <AddIcon style={{ color: 'purple'}}/>
+            </button>
+        </div>
+    </div>
+    )
+}
--- src/Componentes/ItemListContainer.js
+++ src/Componentes/ItemListContainer.js
@@ -1,9 +1,24 @@
 import React from 'react'; 
+import {useState} from 'react'; 
+import ItemCount from '../Componentes/ItemCount';
+import '../Styles/itemCount.css';
+
 
 export default function ItemListContainer(){
+
+    const[cantidad, setCantidad] = useState(1);
+
+    const handleCantidad = (cantidad) => {
+        setCantidad(cantidad);
+    }
+
     return(
     <>
         <h1 style={{ textAlign: 'center', color: "grey"}}>Bienvenido/a a Learn!</h1>
+        <ItemCount stock={5} initial={1} onAdd={handleCantidad}/>
+        <div className="AddCart">
+            <button>Agregar al Carro ({cantidad})</button>
+        </div>
     </>
     )
 };
