c9c19644a2bc6f75758d7e84b3a4115043571cb4
dstefaniapaniagua <deb.s.paniagua@gmail.com>
2021-05-25 20:31:21 -0300

    modified ItemList

--- src/Componentes/ItemList.js
+++ src/Componentes/ItemList.js
@@ -8,21 +8,21 @@ export default function ItemList () {
     const books = [ 
         { id: 1,
         title: "El psicoanálisis en debate", 
-        description: "Una lectura sobre las problemáticas del psicoanálisis y de la salud mental." ,
+        description: "Una lectura sobre las problemáticas del psicoanálisis y de la salud mental.",
         price: "$500", 
         img:'./assets/1.jpg'
     },
 
     {   id: 2,
         title: " Del síntoma al fantasma, Y retorno", 
-        description: "Curso de 25 capítulos tuvo un antecedente que no puede pasar desapercibido. " ,
+        description: "Curso de 25 capítulos tuvo un antecedente que no puede pasar desapercibido. ",
         price: "$800", 
         img:"./assets/2.jpg"
     },
 
     {   id: 3,
         title: "Ser analista hoy", 
-        description: "Permite reubicar el campo del psicoanálisis en el de las prácticas sociales de la actualidad." ,
+        description: "Permite reubicar el campo del psicoanálisis en el de las prácticas sociales de la actualidad.",
         price: "$600", 
         img: './assets/3.jpg'
     }
