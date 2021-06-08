d83260c50850b30b1cfe398666d5f60b7eb5feb1
dstefaniapaniagua <deb.s.paniagua@gmail.com>
2021-06-08 00:42:36 -0300

    modified dataProductos

---
+++ src/utils/dataProductos.js
@@ -0,0 +1,103 @@
+import masvendidosimagen1 from "../assets/1.jpg";
+import psicoanalisisimg2 from "../assets/2.jpg";
+import psicoanalisisimg3 from "../assets/3.jpg";
+import masvendidosimagen4 from "../assets/4.png";
+import psicoanalisisimg5 from "../assets/5.jpg";
+import psicoanalisisimg6 from "../assets/6.jpg";
+import psicoanalisisimg7 from "../assets/7.jpg";
+import masvendidosimagen8 from "../assets/8.jpg";
+import masvendidosimagen9 from "../assets/9.jpg";
+import masvendidosimagen10 from "../assets/10.jpg";
+
+const data = [
+  {
+    id: "1",
+    title: "El psicoanálisis en debate",
+    description:
+      "Una lectura sobre las problemáticas del psicoanálisis y de la salud mental.",
+    price: "$500",
+    img: masvendidosimagen1,
+    category: "masvendidos",
+  },
+
+  {
+    id: "2",
+    title: " Del síntoma al fantasma, Y retorno",
+    description:
+      "Curso de 25 capítulos tuvo un antecedente que no puede pasar desapercibido. ",
+    price: "$800",
+    img: psicoanalisisimg2,
+    category: "psicoanalisis",
+  },
+
+  {
+    id: "3",
+    title: "Ser analista hoy",
+    description:
+      "Sobre el psicoanálisis en las prácticas sociales de la actualidad.",
+    price: "$600",
+    img: psicoanalisisimg3,
+    category: "psicoanalisis",
+  },
+
+  {
+    id: "4",
+    title: "Los cambios que cambian",
+    description: "Sobre la voz en el aula en tiempos actuales",
+    price: "$680",
+    img: masvendidosimagen4,
+    category: "masvendidos",
+  },
+
+  {
+    id: "5",
+    title: "Gabriel Rolon, Historias de Divan",
+    description: "Algo así como diferentes historias",
+    price: "$790",
+    img: psicoanalisisimg5,
+    category: "psicoanalisis",
+  },
+  {
+    id: "6",
+    title: "Nasio, ¡Sí, el psicoanalisis cura",
+
+    description: "Este libro explica sobre la importancia del psiconalisis",
+    price: "$920.99",
+    img: psicoanalisisimg6,
+    category: "psicoanalisis",
+  },
+  {
+    id: "7",
+    title: "Nasio, ¡Sí, el psicoanalisis cura",
+
+    description: "Este libro explica sobre la importancia del psiconalisis",
+    price: "$850",
+    img: psicoanalisisimg7,
+    category: "psicoanalisis",
+  },
+  {
+    id: "8",
+    title: "Vicente Muleiro, La niña de sus ojos",
+    description: "Una novela muy famosa que habla de...",
+    price: "$850",
+    img: masvendidosimagen8,
+    category: "masvendidos",
+  },
+  {
+    id: "9",
+    title: "Pedro Lemebel, La esquina de mi corazón",
+    description: "Una novela que trata sobre las contradicciones del amor",
+    img: masvendidosimagen9,
+    category: "masvendidos",
+  },
+
+  {
+    id: "10",
+    title: "Luis Landero, El huerto de Emerson",
+    description: "Un escrito que retoma las grandes historias de Emerson",
+    img: masvendidosimagen10,
+    category: "masvendidos",
+  },
+];
+
+export default data;
