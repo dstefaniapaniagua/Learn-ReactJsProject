1ab2142f0f1d52d1033a11c32ea3978031d48f09
dstefaniapaniagua <deb.s.paniagua@gmail.com>
2021-06-13 13:50:32 -0300

    modified Navbar

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import { AppBar, Toolbar, Button} from "@material-ui/core";
-import '../Styles/navbar.css';
-import CartWidget from './CartWidget';
-import ContainerMenu from './ContainerMenu';
-import { NavLink, Link} from 'react-router-dom';
-
+import React from "react";
+import { AppBar, Toolbar, Button } from "@material-ui/core";
+import "../Styles/navbar.css";
+import CartWidget from "./CartWidget";
+import ContainerMenu from "./ContainerMenu";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-
-    return (
-        <AppBar className="header" position="static" style={{ background: "grey", display:"inline-block"}}>
-            <Toolbar className="toolbar"> 
-            <Link to="/" className='links-nav' style={{ variant: "h1", fontWeight: "bold", marginLeft: 130}}>
-                LEARN
-            </Link> 
-            <div style={{ marginLeft: "auto"}} >
-                <ContainerMenu />
-                <CartWidget />
-                <Button id="login" style={{ marginLeft: 40,background: "black", color: "white"}}>Login</Button>
-            </div>
-            </Toolbar>
-        </AppBar> 
-    );
+  return (
+    <AppBar
+      className="header"
+      position="static"
+      style={{ background: "grey", display: "inline-block" }}
+    >
+      <Toolbar className="toolbar">
+        <Link
+          to="/"
+          className="links-nav"
+          style={{ variant: "h1", fontWeight: "bold", marginLeft: 130 }}
+        >
+          LEARN
+        </Link>
+        <div style={{ marginLeft: "auto" }}>
+          <ContainerMenu />
+          <CartWidget />
+          <Button
+            id="login"
+            style={{ marginLeft: 40, background: "black", color: "white" }}
+          >
+            Login
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 export default NavBar;
